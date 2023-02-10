676e45618beb595fdbf7d674183b963a48d4db5c
Douglas  Rodrigues <douglasrodrigues@Mac-mini-de-Douglas.local>
2023-02-10 20:14:50 -0300

    Initial Commit

---
+++ CaixaEletronico.playground/Contents.swift
@@ -0,0 +1,43 @@
+import UIKit
+
+// [50, 10, 5, 2]
+
+var value = 117
+
+var valor50 = 50
+var valor10 = 10
+var valor5 = 5
+var valor2 = 2
+
+if valor50 >= valor50 {
+    valor50 = value / 50
+    value = value % 50
+    if valor50 != 0 {
+        print("\(valor50) notas de R$ 50.00")
+    }
+}
+
+if valor10 >= valor10 {
+    valor10 = value / 10
+    value = value % 10
+    if valor10 != 0 {
+        print("\(valor10) notas de R$ 10.00")
+    }
+}
+
+if valor5 >= valor5 {
+    valor5 = value / 5
+    value = value % 5
+    if valor5 != 0 {
+        print("\(valor5) notas de R$ 5.00")
+    }
+}
+
+if valor2 >= valor2 {
+    valor2 = value / 2
+    value = value % 2
+    if valor2 != 0 {
+        print("\(valor2) notas de R$ 2.00")
+    }
+}
+
