d5523f28617a2e4287e219e82f28711265213f2d
Douglas  Rodrigues <douglasrodrigues@Mac-mini-de-Douglas.local>
2023-02-13 17:45:21 -0300

    Correcao numero 11

--- CaixaEletronico.playground/Contents.swift
+++ CaixaEletronico.playground/Contents.swift
@@ -2,7 +2,7 @@ import UIKit
 
 // [50, 10, 5, 2]
 
-var value = 8
+var value = 13
 
 var valor50 = 50
 var valor10 = 10
@@ -18,19 +18,38 @@ if valor50 >= valor50 {
 }
 
 if valor10 >= valor10 {
-    valor10 = value / 10
-    value = value % 10
-    if valor10 != 0 {
-        print("\(valor10) notas de R$ 10.00")
+    if value % 2 == 0 {
+        valor10 = value / 10
+        value = value % 10
+        if valor10 != 0 {
+            print("\(valor10) notas de R$ 10.00")
+        }
     }
 }
 
 if valor5 >= valor5 {
+    var valueOri = value
+    var val5 = 5
     if value % 2 != 0 {
         valor5 = value / 5
         value = value % 5
-        if valor5 != 0 {
-            print("\(valor5) notas de R$ 5.00")
+        
+        if value != 0 {
+            valueOri = valueOri - 5
+            var recebe = valueOri
+            val5 = valueOri / 5
+            valueOri = valueOri % 5
+            print("\(val5) notas de R$ 5.00")
+            
+            var val2 = recebe / 2
+            val2 % 2
+            print("\(val2) notas de R$ 2.00")
+            value = 0
+        }
+        else {
+            if valor5 != 0 {
+                print("\(valor5) notas de R$ 5.00")
+            }
         }
     }
 }
@@ -42,4 +61,3 @@ if valor2 >= valor2 {
         print("\(valor2) notas de R$ 2.00")
     }
 }
-
